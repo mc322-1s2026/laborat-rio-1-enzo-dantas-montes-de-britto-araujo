559d0e5b62ba6b4839829b0de5ac4d27268b3f25
Caique Paiva <caicassopaiva4@gmail.com>
2026-03-16 08:03:14 -0300

    Implementacao Inicial de User

--- src/main/java/com/nexus/model/User.java
+++ src/main/java/com/nexus/model/User.java
@@ -1,5 +1,7 @@
 package com.nexus.model;
 
+import java.util.List;
+
 public class User {
     private final String username;
     private final String email;
@@ -9,8 +11,14 @@ public class User {
             throw new IllegalArgumentException("Username não pode ser vazio.");
         }
         this.username = username;
+        if(email == null || email.isBlank()){
+            throw new IllegalArgumentException("Email não pode ser vazio.");
+        }
+        if(!email.contains("@") && !email.endsWith(".com")){
+            throw new IllegalArgumentException("Email precisa conter @ e terminar com .com");
+        }
         this.email = email;
-    }
+    }   
 
     public String consultEmail() {
         return email;
@@ -20,7 +28,9 @@ public class User {
         return username;
     }
 
-    public long calculateWorkload() {
-        return 0; 
+    public long calculateWorkload(List<Task> lista) {
+        return lista.stream()
+            .filter(t -> t.getStatus() == TaskStatus.IN_PROGRESS)
+            .count(); 
     }
 }
